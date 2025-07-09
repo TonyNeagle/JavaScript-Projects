6cd7144f6de2b93676384ba06d7faf4fee714a16
Tony <tony.neagle@btopenworld.com>
2025-07-09 14:05:03 +0100

    Basic JavaScripts Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+var A = "Hello World!";                     // Declares a variable A and assign it a string value
+var B = " Here he comes to wreck the day";   // Declare sa variable B and assign it a string value
+window.alert(A);                            // Displays the value of A in an alert box
+document.writeln(A + B);        // Writes the values of A and B to the document, separated by a comma and space
+document.writeln(32 * 2);                // An example of an Exporession which writes the result of the multiplication of 32 and 2 to the document
+
