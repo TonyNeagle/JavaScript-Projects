395c8bf44ca8fb4faa196cbf599d20802dcdbe15
Tony <tony.neagle@btopenworld.com>
2025-09-13 13:50:09 +0100

    Add Pizza Project with HTML, CSS, and JS files
    Introduced a new Pizza Project including Pizza.html for the UI, Pizza.css for styling, and Pizza.js for order logic. The project allows users to select pizza size and toppings, calculates the total price, and displays the order summary. Also added .DS_Store files generated by macOS.

---
+++ Basic JavaScript Projects/Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,62 @@
+function getReceipt() {
+    // This initializes our string so it can get passed from 
+    // function to function, growing line by line into a full receipt
+    var text1 = "<h3>You Ordered:</h3>";            // initialises  a variable 
+    var runningTotal = 0;                           // tracks the cummulative cost
+    var sizeTotal = 0;                              // variable initialised to store the cost of selected pissa size
+    var sizeArray = document.getElementsByClassName("size");        // retrieves all elements with the class "size"
+    for (var i = 0; i < sizeArray.length; i++) {                    // for loop to check for selected size of pizza
+        if (sizeArray[i].checked) {
+            var selectedSize = sizeArray[i].value;
+            text1 = text1 + selectedSize + "<br>";
+        }
+    }
+    if (selectedSize === "Personal Pizza") {                        // if else statements to determine the cost according to size
+        sizeTotal = 6;                                              // small pizza = 8, medium pizza = 10 etc. 
+    }   else if (selectedSize === "Small Pizza") {
+        sizeTotal = 8;
+    } else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10;
+    } else if (selectedSize === "Large Pizza") {
+        sizeTotal = 14;
+    } else if (selectedSize === "Extra Large Pizza") {
+        sizeTotal = 16;
+    }
+    runningTotal = sizeTotal;                                       // after size is selected it is added to the running total
+    console.log(selectedSize + " = $" + sizeTotal + ".00");
+    console.log("size text1: " + text1);
+    console.log("subtotal: $" + runningTotal + ".00");
+    // These variables will get passed on to each function
+    getTopping(runningTotal, text1);
+}
+
+function getTopping(runningTotal, text1) {                             // calls the get topping function to add the selected toppings to the running total
+    var toppingTotal = 0;
+    var selectedTopping = [];
+    var toppingArray = document.getElementsByClassName("toppings");
+    for (var j = 0; j < toppingArray.length; j++) {
+        if (toppingArray[j].checked) {
+            selectedTopping.push(toppingArray[j].value);
+            console.log("selected topping item: (" + toppingArray[j].value + ")");
+            text1 = text1 + toppingArray[j].value + "<br>";
+        }
+    }
+
+    var toppingCount = selectedTopping.length;
+    if (toppingCount > 1) {
+        toppingTotal = (toppingCount - 1);
+    } else {
+        toppingTotal = 0;
+    }
+    runningTotal = (runningTotal + toppingTotal);
+    console.log("total selected topping items: " + toppingCount);
+    console.log(toppingCount + " topping - 1 free topping = $" + toppingTotal + ".00");
+    console.log("topping text1: " + text1);
+    console.log("Purchase Total: $" + runningTotal + ".00");
+    document.getElementById("showText").innerHTML = text1;
+    document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$" + runningTotal + ".00" + "</strong></h3>";
+}
+
+
+
+
